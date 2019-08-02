2af8aaf07252e451c7c3e336f4b29af49adb8bde
Roberto Varelo <51290642+rvarelo@users.noreply.github.com>
2019-08-02 06:19:03 -0300

    curso php
    curso php

---
+++ _inserir_produto.php
@@ -0,0 +1,12 @@
+<?php 
+
+$nroproduto = $_POST["nroproduto"];
+$nomeproduto = $_POST["nomeproduto"];
+$categoria = $_POST["categoria"];
+$quantidade = $_POST["quantidade"];
+$fornecedor = $_POST["fornecedor"];
+
+echo "$nroproduto, $nomeproduto,$categoria,$quantidade,$fornecedor"; 
+
+
+ ?>
